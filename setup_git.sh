3f3f786cddaf33d3eee5af3e8b968008de865277
gaura <gauraboschma@gmail.com>
2023-10-27 20:01:36 +0200

    it ask for sudo for tee

--- setup_git.sh
+++ setup_git.sh
@@ -4,18 +4,18 @@ set -e
 set -u
 
 log_file=/tmp/setup_git.log
-
+teesudo() { sudo tee -a "$log_file"; }
 log_and_run() {
     cmd="$1"
-    echo "Executing: $cmd" | tee -a "$log_file"
+    echo "Executing: $cmd" | teesudo
 
     set +e
-    eval "$cmd" 2>&1 | tee -a "$log_file"
+    eval "$cmd" 2>&1 | teesudo
     cmd_exit_status=$?
     set -e
 
     if [ $cmd_exit_status -ne 0 ]; then
-        echo "Command failed with exit status $cmd_exit_status: $cmd" | tee -a "$log_file"
+        echo "Command failed with exit status $cmd_exit_status: $cmd" | teesudo
         echo "cat /tmp/setup_git.log"
         echo "nvim /tmp/setup_git.log"
 
@@ -23,70 +23,70 @@ log_and_run() {
     fi
 }
 
-echo "---------------------------------------------------------------" | tee -a "$log_file"
-echo "$(date) Starting the Setup for Git & GitHub CLI..." | tee -a "$log_file"
-echo "---------------------------------------------------------------" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
-echo "----------------------------------------------------------------" | tee -a "$log_file"
-echo "Created a log file in the same directory of this script location" | tee -a "$log_file"
-echo "----------------------------------------------------------------" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
+echo "---------------------------------------------------------------" | teesudo
+echo "$(date) Starting the Setup for Git & GitHub CLI..." | teesudo
+echo "---------------------------------------------------------------" | teesudo
+echo "" | teesudo
+echo "" | teesudo
+echo "" | teesudo
+echo "----------------------------------------------------------------" | teesudo
+echo "Created a log file in the same directory of this script location" | teesudo
+echo "----------------------------------------------------------------" | teesudo
+echo "" | teesudo
+echo "" | teesudo
+echo "" | teesudo
 
 # Prompt user for name and email
-echo "Enter your username: "   | tee -a "$log_file"
+echo "Enter your username: "   | teesudo
 read -r username && log_and_run "echo 'Name acquired'"
-echo "Enter your email: "  | tee -a "$log_file"
+echo "Enter your email: "  | teesudo
 read -r email && log_and_run "echo 'Email acquired'"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
+echo "" | teesudo
+echo "" | teesudo
+echo "" | teesudo
 
 # Set the provided name and email for git
 git config --global user.name "$username" && log_and_run "echo 'Set the git global username'"
 git config --global user.email "$email" && log_and_run "echo 'Set the git global email'"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
+echo "" | teesudo
+echo "" | teesudo
+echo "" | teesudo
 
 # Generate SSH key
 ssh-keygen -t ed25519 -C "$email"  && log_and_run "echo 'Set the git global email'"
 ssh_key_path="$HOME/.ssh/id_ed25519" && log_and_run "echo 'Set the git global email'"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
+echo "" | teesudo
+echo "" | teesudo
+echo "" | teesudo
 
 # Start the ssh-agent and load the SSH key
 eval "$(ssh-agent -s)" && log_and_run "echo 'Started the ssh-agent'"
 ssh-add "$ssh_key_path" && log_and_run "echo 'Added the SSH Key'"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
+echo "" | teesudo
+echo "" | teesudo
+echo "" | teesudo
 
 # Authenticate GitHub CLI
 if command -v gh > /dev/null 2>&1; then
-    echo "GitHub Cli is installed" | tee -a "$log_file"
+    echo "GitHub Cli is installed" | teesudo
     gh auth login -s 'user:email,read:org,repo,write:org,notifications' -p ssh && log_and_run "echo 'Logged in to GitHub'"
-    echo "" | tee -a "$log_file"
-    echo "" | tee -a "$log_file"
-    echo "" | tee -a "$log_file"
+    echo "" | teesudo
+    echo "" | teesudo
+    echo "" | teesudo
 else
-    echo "------------------------------------------------------------" | tee -a "$log_file"
-    echo "See the Linux/BSD page for distro speciffic instuctions"      | tee -a "$log_file"
-    echo "https://github.com/cli/cli/blob/trunk/docs/install_linux.md"  | tee -a "$log_file"
-    echo "------------------------------------------------------------" | tee -a "$log_file"
-    echo "GitHub Cli is not installed" | tee -a "$log_file"
+    echo "------------------------------------------------------------" | teesudo
+    echo "See the Linux/BSD page for distro speciffic instuctions"      | teesudo
+    echo "https://github.com/cli/cli/blob/trunk/docs/install_linux.md"  | teesudo
+    echo "------------------------------------------------------------" | teesudo
+    echo "GitHub Cli is not installed" | teesudo
     exit 1
 fi
 
 # Test the SSH connection to GitHub
 log_and_run "ssh -T git@github.com"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
-echo "" | tee -a "$log_file"
+echo "" | teesudo
+echo "" | teesudo
+echo "" | teesudo
 
 # rm ./setup_git.log
 # echo "--------------------"
