1b17ae4f6718b2ea278795385bfc432fa9c6b0d4
gaura <gauraboschma@gmail.com>
2023-10-27 23:37:09 +0200

    trying new things

--- setup_git.sh
+++ setup_git.sh
@@ -1,101 +1,124 @@
 #!/usr/bin/env sh
-
-set -e
-set -u
-
-log_file=/tmp/setup_git.log
-teesudo() { sudo tee -a "$log_file"; }
-log_and_run() {
-    cmd="$1"
-    echo "Executing: $cmd" | teesudo
-
-    set +e
-    eval "$cmd" 2>&1 | teesudo
-    cmd_exit_status=$?
+if [ -z "$test_setup" ]; then
     set -e
+    set -u
 
-    if [ $cmd_exit_status -ne 0 ]; then
-        echo "Command failed with exit status $cmd_exit_status: $cmd" | teesudo
-        echo "cat /tmp/setup_git.log"
-        echo "nvim /tmp/setup_git.log"
+    tmp_log_file=$(mktemp)
 
+    teesudo() { sudo tee -a "$tmp_log_file"; }
+    teetmp() { tee -a "$tmp_log_file"; }
+
+    readeable_log_file_before() {
+        echo "" | teesudo
+        echo "" | teesudo
+        echo "---------------------------------------------------------------" | teesudo
+    }
+
+    readeable_log_file_after() {
+        echo "---------------------------------------------------------------" | teesudo
+        echo "" | teesudo
+        echo "" | teesudo
+    }
+
+
+    log_and_run() {
+        cmd="$1"
+        readeable_log_file_before
+        echo "Executing: $cmd" | teesudo
+        readeable_log_file_after
+
+        set +e
+        eval "$cmd" 2>&1 | teesudo
+        cmd_exit_status=$?
+        set -e
+
+        if [ $cmd_exit_status -ne 0 ]; then
+            echo "Command failed with exit status $cmd_exit_status: $cmd" | teesudo
+            mv "$tmp_log_file" ./setup_git.log
+
+            echo "cat ./setup_git.log"
+            echo "nvim ./setup_git.log"
+
+            exit 1
+        fi
+    }
+
+    readeable_log_file_before
+    echo "$(date) Starting the Setup for Git & GitHub CLI..." | teesudo
+    readeable_log_file_after
+
+    readeable_log_file_before
+    echo "Created a log file in the same directory of this script location" | teesudo
+    readeable_log_file_after
+
+    # Prompt user for name and email only if they are not provided
+    if [ -z "$username" ]; then
+        readeable_log_file_before
+        read -rp "Enter your username: "  username && log_and_run "echo 'Name acquired'"
+        readeable_log_file_after
+    fi
+    if [ -z "$email" ]; then
+        readeable_log_file_before
+        read -rp "Enter your email: "  username && log_and_run "echo 'Email acquired'"
+        readeable_log_file_after
+    fi
+
+    # Set the provided name and email for git
+    readeable_log_file_before
+    git config --global user.name "$username" && log_and_run "echo 'Set the git global username'"
+    readeable_log_file_after
+
+    readeable_log_file_before
+    git config --global user.email "$email" && log_and_run "echo 'Set the git global email'"
+    readeable_log_file_after
+
+    # Generate SSH key
+    ssh_key_path="$HOME/.ssh/id_ed25519"
+    if [ ! -f "$ssh_key_path" ]; then
+        readeable_log_file_before
+        ssh-keygen -t ed25519 -C "$email"  && log_and_run "echo 'Set the git global email'"
+        readeable_log_file_after
+        # Start the ssh-agent and load the SSH key
+        readeable_log_file_before
+        eval "$(ssh-agent -s)" && log_and_run "echo 'Started the ssh-agent'"
+        readeable_log_file_after
+
+        readeable_log_file_before
+        ssh-add "$ssh_key_path" && log_and_run "echo 'Added the SSH Key'"
+        readeable_log_file_after
+    fi
+
+
+    # Authenticate GitHub CLI
+    if command -v gh > /dev/null 2>&1; then
+        readeable_log_file_before
+        echo "GitHub Cli is installed" | teesudo
+        readeable_log_file_after
+
+        readeable_log_file_before
+        gh auth login -s 'user:email,read:org,repo,write:org,notifications' -p ssh && log_and_run "echo 'Logged in to GitHub'"
+        readeable_log_file_after
+    else
+        readeable_log_file_before
+        echo "See the Linux/BSD page for distro speciffic instuctions"      | teesudo
+        echo "https://github.com/cli/cli/blob/trunk/docs/install_linux.md"  | teesudo
+        echo "GitHub Cli is not installed" | teesudo
+        readeable_log_file_after
         exit 1
     fi
-}
 
-echo "---------------------------------------------------------------" | teesudo
-echo "$(date) Starting the Setup for Git & GitHub CLI..." | teesudo
-echo "---------------------------------------------------------------" | teesudo
-echo "" | teesudo
-echo "" | teesudo
-echo "" | teesudo
-echo "----------------------------------------------------------------" | teesudo
-echo "Created a log file in the same directory of this script location" | teesudo
-echo "----------------------------------------------------------------" | teesudo
-echo "" | teesudo
-echo "" | teesudo
-echo "" | teesudo
+    # Test the SSH connection to GitHub
+    readeable_log_file_before
+    log_and_run "ssh -T git@github.com"
+    readeable_log_file_after
 
-# Prompt user for name and email
-echo "Enter your username: "   | teesudo
-read -r username && log_and_run "echo 'Name acquired'"
-echo "Enter your email: "  | teesudo
-read -r email && log_and_run "echo 'Email acquired'"
-echo "" | teesudo
-echo "" | teesudo
-echo "" | teesudo
-
-# Set the provided name and email for git
-git config --global user.name "$username" && log_and_run "echo 'Set the git global username'"
-git config --global user.email "$email" && log_and_run "echo 'Set the git global email'"
-echo "" | teesudo
-echo "" | teesudo
-echo "" | teesudo
-
-# Generate SSH key
-ssh-keygen -t ed25519 -C "$email"  && log_and_run "echo 'Set the git global email'"
-ssh_key_path="$HOME/.ssh/id_ed25519" && log_and_run "echo 'Set the git global email'"
-echo "" | teesudo
-echo "" | teesudo
-echo "" | teesudo
-
-# Start the ssh-agent and load the SSH key
-eval "$(ssh-agent -s)" && log_and_run "echo 'Started the ssh-agent'"
-ssh-add "$ssh_key_path" && log_and_run "echo 'Added the SSH Key'"
-echo "" | teesudo
-echo "" | teesudo
-echo "" | teesudo
-
-# Authenticate GitHub CLI
-if command -v gh > /dev/null 2>&1; then
-    echo "GitHub Cli is installed" | teesudo
-    gh auth login -s 'user:email,read:org,repo,write:org,notifications' -p ssh && log_and_run "echo 'Logged in to GitHub'"
-    echo "" | teesudo
-    echo "" | teesudo
-    echo "" | teesudo
+    readeable_log_file_before
+    echo "Git and SSH have been configured with the provided name and email."
+    readeable_log_file_after
+    exit 0
 else
-    echo "------------------------------------------------------------" | teesudo
-    echo "See the Linux/BSD page for distro speciffic instuctions"      | teesudo
-    echo "https://github.com/cli/cli/blob/trunk/docs/install_linux.md"  | teesudo
-    echo "------------------------------------------------------------" | teesudo
-    echo "GitHub Cli is not installed" | teesudo
-    exit 1
+    username="$(if test -z "$username"; then read -r "Provide your username: " username; fi)"
+    email="$(if test -z "$email"; then read -r "Provide your email: " email; fi)"
+    git config --global user.name "$username"
+    git config --global user.email "$email"
 fi
-
-# Test the SSH connection to GitHub
-log_and_run "ssh -T git@github.com"
-echo "" | teesudo
-echo "" | teesudo
-echo "" | teesudo
-
-# rm ./setup_git.log
-# echo "--------------------"
-# echo "Removing the logfile"
-# echo "--------------------"
-# echo ""
-# echo ""
-# echo ""
-echo "------------------------------------------------------------------"
-echo "Git and SSH have been configured with the provided name and email."
-echo "------------------------------------------------------------------"
-exit 0
