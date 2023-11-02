4f29ec77451427ed1fe7edd605c6aac64a94bff4
gaura <gauraboschma@gmail.com>
2023-11-02 19:20:40 +0100

    made this install process better

--- doas.sh
+++ doas.sh
@@ -45,20 +45,27 @@ else
     esac
 fi
 
+# Get the current effective username
 current_user=$(whoami)
-sudo_user=$SUDO_USER
-user_env_var=$USER
 
-if [ "$current_user" = "root" ] && { [ "$sudo_user" = "root" ] || [ -z "$sudo_user" ]; } && { [ "$user_env_var" = "root" ] || [ -z "$user_env_var" ]; }; then
-    read -rp "Please enter your actual username: " actual_user
-else
-    if [ -n "$SUDO_USER" ]; then
+# Logic to determine the actual username
+if [ "$current_user" = "root" ]; then
+    # If a sudo user exists and is not root, use that
+    if [ -n "$SUDO_USER" ] && [ "$SUDO_USER" != "root" ]; then
         actual_user="$SUDO_USER"
+    # If no sudo user, check the USER environment variable
     elif [ -n "$USER" ] && [ "$USER" != "root" ]; then
         actual_user="$USER"
+    # Lastly, check the USERNAME environment variable
+    elif [ -n "$USERNAME" ] && [ "$USERNAME" != "root" ]; then
+        actual_user="$USERNAME"
     else
-        actual_user="$current_user"
+        # If all else fails, ask the user to enter their username
+        read -rp "Please enter your actual username: " actual_user
     fi
+else
+    # If not running as root, use the current effective user
+    actual_user="$current_user"
 fi
 
 if [ -f /etc/doas.conf ]; then
@@ -66,83 +73,83 @@ if [ -f /etc/doas.conf ]; then
 
     # Loop over each line to append
     while IFS= read -r line; do
-        # Replace USER with actual_user in the line before the grep check
-        # line_replaced=$(echo "$line" | sed "s/USER/$actual_user/g")
-        line_replaced="${line//USER/$actual_user}"
+        # Replace REPLACEMENT_USER_NAME with actual_user in the line before the grep check
+        # line_replaced=$(echo "$line" | sed "s/REPLACEMENT_USER_NAME/$actual_user/g")
+        line_replaced="${line//REPLACEMENT_USER_NAME/$actual_user}"
 
         # Check if the replaced line already exists for the user in doas.conf
         if ! grep -Fxq "$line_replaced" /etc/doas.conf; then
             echo "$line_replaced" | tee -a /etc/doas.conf > /dev/null
         fi
     done <<EOF
-permit persist keepenv USER as root
-permit nopass keepenv USER as root cmd virt-clone
-permit nopass keepenv USER as root cmd virt-viewer
-permit nopass USER as root cmd apt
-permit nopass USER as root cmd cat
-permit nopass USER as root cmd chmod
-permit nopass USER as root cmd chown
-permit nopass USER as root cmd cp
-permit nopass USER as root cmd find
-permit nopass USER as root cmd grep
-permit nopass USER as root cmd ln
-permit nopass USER as root cmd mkdir
-permit nopass USER as root cmd mount
-permit nopass USER as root cmd mv
-permit nopass USER as root cmd nala
-permit nopass USER as root cmd nvim
-permit nopass USER as root cmd pacman
-permit nopass USER as root cmd poweroff
-permit nopass USER as root cmd rc-service
-permit nopass USER as root cmd rc-update
-permit nopass USER as root cmd reboot
-permit nopass USER as root cmd rm
-permit nopass USER as root cmd sed
-permit nopass USER as root cmd snapctl
-permit nopass USER as root cmd su
-permit nopass USER as root cmd systemctl
-permit nopass USER as root cmd tee
-permit nopass USER as root cmd umount
-permit nopass USER as root cmd unzip
-permit nopass USER as root cmd updatedb
-permit nopass USER as root cmd virsh
-permit nopass USER as root cmd vmctl
+permit persist keepenv REPLACEMENT_USER_NAME as root
+permit nopass keepenv REPLACEMENT_USER_NAME as root cmd virt-clone
+permit nopass keepenv REPLACEMENT_USER_NAME as root cmd virt-viewer
+permit nopass REPLACEMENT_USER_NAME as root cmd apt
+permit nopass REPLACEMENT_USER_NAME as root cmd cat
+permit nopass REPLACEMENT_USER_NAME as root cmd chmod
+permit nopass REPLACEMENT_USER_NAME as root cmd chown
+permit nopass REPLACEMENT_USER_NAME as root cmd cp
+permit nopass REPLACEMENT_USER_NAME as root cmd find
+permit nopass REPLACEMENT_USER_NAME as root cmd grep
+permit nopass REPLACEMENT_USER_NAME as root cmd ln
+permit nopass REPLACEMENT_USER_NAME as root cmd mkdir
+permit nopass REPLACEMENT_USER_NAME as root cmd mount
+permit nopass REPLACEMENT_USER_NAME as root cmd mv
+permit nopass REPLACEMENT_USER_NAME as root cmd nala
+permit nopass REPLACEMENT_USER_NAME as root cmd nvim
+permit nopass REPLACEMENT_USER_NAME as root cmd pacman
+permit nopass REPLACEMENT_USER_NAME as root cmd poweroff
+permit nopass REPLACEMENT_USER_NAME as root cmd rc-service
+permit nopass REPLACEMENT_USER_NAME as root cmd rc-update
+permit nopass REPLACEMENT_USER_NAME as root cmd reboot
+permit nopass REPLACEMENT_USER_NAME as root cmd rm
+permit nopass REPLACEMENT_USER_NAME as root cmd sed
+permit nopass REPLACEMENT_USER_NAME as root cmd snapctl
+permit nopass REPLACEMENT_USER_NAME as root cmd su
+permit nopass REPLACEMENT_USER_NAME as root cmd systemctl
+permit nopass REPLACEMENT_USER_NAME as root cmd tee
+permit nopass REPLACEMENT_USER_NAME as root cmd umount
+permit nopass REPLACEMENT_USER_NAME as root cmd unzip
+permit nopass REPLACEMENT_USER_NAME as root cmd updatedb
+permit nopass REPLACEMENT_USER_NAME as root cmd virsh
+permit nopass REPLACEMENT_USER_NAME as root cmd vmctl
 EOF
 else
     echo "Creating doas.conf"
-    cat <<EOF | sed "s/USER/$actual_user/g" | tee -a /etc/doas.conf
-permit persist keepenv USER as root
-permit nopass keepenv USER as root cmd virt-clone
-permit nopass keepenv USER as root cmd virt-viewer
-permit nopass USER as root cmd apt
-permit nopass USER as root cmd cat
-permit nopass USER as root cmd chmod
-permit nopass USER as root cmd chown
-permit nopass USER as root cmd cp
-permit nopass USER as root cmd find
-permit nopass USER as root cmd grep
-permit nopass USER as root cmd ln
-permit nopass USER as root cmd mkdir
-permit nopass USER as root cmd mount
-permit nopass USER as root cmd mv
-permit nopass USER as root cmd nala
-permit nopass USER as root cmd nvim
-permit nopass USER as root cmd pacman
-permit nopass USER as root cmd poweroff
-permit nopass USER as root cmd rc-service
-permit nopass USER as root cmd rc-update
-permit nopass USER as root cmd reboot
-permit nopass USER as root cmd rm
-permit nopass USER as root cmd sed
-permit nopass USER as root cmd snapctl
-permit nopass USER as root cmd su
-permit nopass USER as root cmd systemctl
-permit nopass USER as root cmd tee
-permit nopass USER as root cmd umount
-permit nopass USER as root cmd unzip
-permit nopass USER as root cmd updatedb
-permit nopass USER as root cmd virsh
-permit nopass USER as root cmd vmctl
+    cat <<EOF | sed "s/REPLACEMENT_USER_NAME/$actual_user/g" | tee -a /etc/doas.conf
+permit persist keepenv REPLACEMENT_USER_NAME as root
+permit nopass keepenv REPLACEMENT_USER_NAME as root cmd virt-clone
+permit nopass keepenv REPLACEMENT_USER_NAME as root cmd virt-viewer
+permit nopass REPLACEMENT_USER_NAME as root cmd apt
+permit nopass REPLACEMENT_USER_NAME as root cmd cat
+permit nopass REPLACEMENT_USER_NAME as root cmd chmod
+permit nopass REPLACEMENT_USER_NAME as root cmd chown
+permit nopass REPLACEMENT_USER_NAME as root cmd cp
+permit nopass REPLACEMENT_USER_NAME as root cmd find
+permit nopass REPLACEMENT_USER_NAME as root cmd grep
+permit nopass REPLACEMENT_USER_NAME as root cmd ln
+permit nopass REPLACEMENT_USER_NAME as root cmd mkdir
+permit nopass REPLACEMENT_USER_NAME as root cmd mount
+permit nopass REPLACEMENT_USER_NAME as root cmd mv
+permit nopass REPLACEMENT_USER_NAME as root cmd nala
+permit nopass REPLACEMENT_USER_NAME as root cmd nvim
+permit nopass REPLACEMENT_USER_NAME as root cmd pacman
+permit nopass REPLACEMENT_USER_NAME as root cmd poweroff
+permit nopass REPLACEMENT_USER_NAME as root cmd rc-service
+permit nopass REPLACEMENT_USER_NAME as root cmd rc-update
+permit nopass REPLACEMENT_USER_NAME as root cmd reboot
+permit nopass REPLACEMENT_USER_NAME as root cmd rm
+permit nopass REPLACEMENT_USER_NAME as root cmd sed
+permit nopass REPLACEMENT_USER_NAME as root cmd snapctl
+permit nopass REPLACEMENT_USER_NAME as root cmd su
+permit nopass REPLACEMENT_USER_NAME as root cmd systemctl
+permit nopass REPLACEMENT_USER_NAME as root cmd tee
+permit nopass REPLACEMENT_USER_NAME as root cmd umount
+permit nopass REPLACEMENT_USER_NAME as root cmd unzip
+permit nopass REPLACEMENT_USER_NAME as root cmd updatedb
+permit nopass REPLACEMENT_USER_NAME as root cmd virsh
+permit nopass REPLACEMENT_USER_NAME as root cmd vmctl
 EOF
 fi
 
